84fcadb1a044c3ab928747a79f576d2977e9c733
Mohammed Al-Rawi <malrawi@cvc.uab.es>
2019-04-17 19:02:24 +0200

    Added probability of augment, color and invert

--- datasets.py
+++ datasets.py
@@ -9,19 +9,6 @@ import torchvision.transforms as transforms
 import numpy as np
 import cv2
 
-#class Invert_B_img(object): # to be used for the inversion of B image, if needed
-#    """ Does nothing to the image, to be used instead of None    
-#    Args:
-#        image in, image out, nothing is done
-#    """
-#        
-#    def __call__(self, image):                         
-#        image = image.point(lambda p: 255-p if p>8 and p<64 else 0 ) # invert         
-##        
-#        
-#        return image
-#
-
 def color_mapping(img, Cr, Cg, Cb, ignor_zeros=False):
     #  option is to use pallet, imageWithColorPalette = colorImage.convert("P", palette=Image.ADAPTIVE, colors=8)
     # or, option to use this conversion https://realpython.com/python-opencv-color-spaces/
@@ -57,7 +44,8 @@ def color_mapping_cv2(img):
 
 
 class ImageDataset(Dataset):
-    def __init__(self, root, transform=None, unaligned=False, mode='train', gt=False, p_color_augment=0):
+    def __init__(self, root, transform=None, unaligned=False, mode='train', 
+                 gt=False, p_color_augment=0, p_RGB2BGR_augment=0, p_invert_augment=0):
         '''Args-
         gt=True, loads the white Ground Truth pixel level text, if False, use the colored  
         p_color_augment: The probability used to do color augmentation, if p is 0, no augmentaion is 
@@ -68,6 +56,9 @@ class ImageDataset(Dataset):
         self.mode = mode
         self.unaligned = unaligned
         self.p_color_augment = p_color_augment
+        self.p_RGB2BGR_augment = p_RGB2BGR_augment
+        self.p_invert_augment = p_invert_augment
+        
 
         self.files_A = sorted(glob.glob(os.path.join(root, '%s/A' % mode) + '/*.*'))
         if not gt:
@@ -91,10 +82,10 @@ class ImageDataset(Dataset):
                 Cr, Cg, Cb = np.random.randint(0, 256, 3) # One coeff for each band, we will used one coeff for all            
                 item_A = color_mapping(item_A, Cr, Cg, Cb)
                 item_B = color_mapping(item_B, Cr, Cg, Cb, ignor_zeros=True)                
-            elif np.random.rand()>0.66:
+            elif np.random.rand()>self.p_RGB2BGR_augment:
                 item_A = color_mapping_cv2(item_A)
                 item_B = color_mapping_cv2(item_B)                
-            elif np.random.rand()>0.66:
+            elif np.random.rand()>self.p_invert_augment:
                 item_A = PIL_invert(item_A)                
                 item_B = item_B.point(lambda p: 255-p if p>0 else 0 ) # invert                
                                     
@@ -110,3 +101,27 @@ class ImageDataset(Dataset):
 
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
+
+
+
+
+#class NoneTransform(object):
+#    """ Does nothing to the image, to be used instead of None    
+#    Args:
+#        image in, image out, nothing is done
+#    """
+#        
+#    def __call__(self, image):       
+#        return image
+#
+#class Invert_B_img(object):
+#    """ Does nothing to the image, to be used instead of None    
+#    Args:
+#        image in, image out, nothing is done
+#    """
+#        
+#    def __call__(self, image):                         
+#        image = image.point(lambda p: 255-p if p>8 and p<64 else 0 ) # invert         
+##        
+#        
+#        return image
